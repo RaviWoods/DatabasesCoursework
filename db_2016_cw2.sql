d18f0217998bddb41eae11eec49ac647e3250c25
Ravi Woods <rbw14@vm-shell1.doc.ic.ac.uk>
2016-02-14 23:11:57 +0000

    Q1, Q2 done

--- db_2016_cw2.sql
+++ db_2016_cw2.sql
@@ -1,10 +1,19 @@
 -- Q1 returns (name,name,born_in)
+PRINT 'Q1';
+SELECT		person_a.name, person_b.name, person_a.born_in
+FROM		person AS person_c
+JOIN		person AS person_a
+ON		person_a.name = person_c.father
+JOIN		person AS person_b
+ON 		person_b.name = person_c.mother
+AND		person_b.born_in = person_a.born_in;
 
-;
 
 -- Q2 returns (name)
-
-;
+PRINT 'Q2';
+SELECT	      name
+FROM	      monarch
+WHERE	      coronation IS null;
 
 -- Q3 returns (name,father,mother)
 
