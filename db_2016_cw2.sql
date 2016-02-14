95aecf9e7ad1bc6594627db8de3f0e4ae4c20bd1
Ravi <ravi.woods@gmail.com>
2016-02-14 21:44:49 +0000

    INIT

---
+++ db_2016_cw2.sql
@@ -0,0 +1,24 @@
+-- Q1 returns (name,name,born_in)
+
+;
+
+-- Q2 returns (name)
+
+;
+
+-- Q3 returns (name,father,mother)
+
+; 
+
+-- Q4 returns (name)
+
+;
+
+-- Q5 returns (name)
+
+;
+
+-- Q6 returns (house,name,accession)
+
+;
+
