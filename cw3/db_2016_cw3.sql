aeaa8270b5020a145ee4a0049296dfaeb7992d1b
Ravi Woods <rbw14@vm-shell1.doc.ic.ac.uk>
2016-02-25 01:24:25 +0000

    DONE ALL 6

--- cw3/db_2016_cw3.sql
+++ cw3/db_2016_cw3.sql
@@ -32,43 +32,91 @@ SELECT
 		CASE 
 			WHEN EXTRACT(year FROM accession) BETWEEN 1600 AND 1700
 			THEN 1 
-			ELSE 0 
+			ELSE NULL 
 		END) 
 	AS seventeenth,
-	(CASE
-                WHEN EXTRACT(year FROM accession) BETWEEN 1700 AND 1800
-                THEN 1
-                ELSE 0
-        END) AS eighteenth,
-	(CASE
-                WHEN EXTRACT(year FROM accession) BETWEEN 1800 AND 1900
-                THEN 1
-                ELSE 0
-        END) AS nineteenth,
-	(CASE
-                WHEN EXTRACT(year FROM accession) BETWEEN 1900 AND 2000
-                THEN 1
-                ELSE 0
-        END) AS twentieth
+	COUNT(
+		CASE 
+			WHEN EXTRACT(year FROM accession) BETWEEN 1700 AND 1800
+			THEN 1
+			ELSE NULL 
+		END) 
+	AS eighteenth,
+	COUNT(
+		CASE 
+			WHEN EXTRACT(year FROM accession) BETWEEN 1800 AND 1900
+			THEN 1
+			ELSE NULL 
+		END) 
+	AS nineteenth,
+	COUNT(
+		CASE 
+			WHEN (EXTRACT(year FROM accession) > '1900')
+			THEN 1 
+			ELSE NULL
+		END) 
+	AS twentieth
 FROM	
 	monarch 
 WHERE	
 	house IS NOT null
 GROUP BY
-	house, accession 
+	house 
 ORDER BY
         house ASC;
 
 
 -- Q4 returns (name,age)
-
-;
+SELECT 
+	parent.name,
+	MIN( EXTRACT(year FROM child.dob) - EXTRACT(year FROM parent.dob) )
+FROM 
+	person AS parent 
+	JOIN person AS child 
+	ON 
+		child.father = parent.name OR child.mother = parent.name
+GROUP BY 
+	parent.name
+ORDER BY
+	parent.name ASC;
 
 -- Q5 returns (father,child,born)
+SELECT 
+	father.name AS father, 
+	child.name AS child,
+	CASE
+		WHEN child.name IS NOT NULL
+		THEN RANK() OVER 
+			(PARTITION BY father.name ORDER BY child.dob DESC) 
+		ELSE NULL
+		END 
+	AS born 
+FROM 
+	person AS father 
+	LEFT JOIN person AS child 
+	ON 
+		father.name = child.father 
+WHERE 
+	father.gender = 'M' 
+ORDER BY 
+	father;
 
-;
 
 -- Q6 returns (monarch,prime_minister)
-
-;
+SELECT DISTINCT
+	monarch.name AS monarch, 
+	prime_minister.name AS prime_minister
+FROM 
+	monarch 
+	JOIN prime_minister 
+	ON 
+		accession < entry 
+		AND 
+		entry < ALL(
+			SELECT accession 
+			FROM monarch AS second_monarch 
+			WHERE second_monarch.accession > monarch.accession)
+ORDER BY 
+	monarch ASC,
+	prime_minister ASC;
 
